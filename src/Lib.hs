0e99d16f40a8046fdab82a2fc8b52ed758e974c5
Alexander Shabalin <shabalyn.a@gmail.com>
2017-01-02 20:47:09 +0300

    Add printing the actual Syntax tree

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,12 +5,13 @@ module Lib
 import Control.Monad (forM_)
 import System.Environment (getArgs)
 
-import BytecodePrinter (prettyPrint)
+import qualified BytecodePrinter as Bytecode
 import qualified BytecodeInterpreter as Bytecode
-import BytecodeTranslator (translate)
+import qualified BytecodeTranslator as Bytecode
 import Eval (eval)
 import qualified JIT
 import Parser (parseExpr)
+import PrettyPrint (prettyPrint)
 import SyntaxResolver (resolve)
 
 evaluateFile :: FilePath -> IO ()
@@ -23,25 +24,27 @@ interpretBytecode :: FilePath -> IO ()
 interpretBytecode fname = do
   contents <- readFile fname
   let expr = resolve $ parseExpr contents
-  let bc = translate expr
+  let bc = Bytecode.translate expr
   Bytecode.interpret bc
 
 jit :: FilePath -> IO ()
 jit fname = do
   contents <- readFile fname
   let expr = resolve $ parseExpr contents
-  let bc = translate expr
+  let bc = Bytecode.translate expr
   JIT.interpret bc
 
 dumpBytecode :: FilePath -> IO ()
 dumpBytecode fname = do
   contents <- readFile fname
   putStrLn $ "\nDoing: " ++ fname
-  putStrLn "======= CODE ======="
+  putStrLn "======= FILE ======="
   putStrLn contents
-  let expr = resolve $ parseExpr contents
+  let expr = parseExpr contents
+  putStrLn "======= CODE ======="
+  putStrLn $ prettyPrint expr
   putStrLn "===== BYTECODE ====="
-  putStrLn $ prettyPrint $ translate expr
+  putStrLn $ Bytecode.prettyPrint $ Bytecode.translate $ resolve expr
 
 getOperation :: [String] -> (FilePath -> IO (), [String])
 getOperation [] = (const $ pure (), [])
