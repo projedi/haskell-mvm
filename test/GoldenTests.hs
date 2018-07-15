169825c0932d3acf462a81da2220e4d2ea67f7a2
Alexander Shabalin <shabalyn.a@gmail.com>
2018-07-15 21:24:19 +0300

    Fix formatting

--- test/GoldenTests.hs
+++ test/GoldenTests.hs
@@ -104,38 +104,26 @@ tests = testGroup "Golden" [passTests, failTests, intensiveTests, graphicsTests]
 
 passTests :: TestTree
 passTests =
-  testGroup
-    "Pass"
-    [ testsWithParams "Dumb" 1 "expected" ["--dumb"] names
-    ]
+  testGroup "Pass" [testsWithParams "Dumb" 1 "expected" ["--dumb"] names]
   where
     names = testList ++ originalTestList
 
 failTests :: TestTree
 failTests =
   expectFail $
-  testGroup
-    "Fail"
-    [ testsWithParams "Dumb" 30 "expected" ["--dumb"] names
-    ]
+  testGroup "Fail" [testsWithParams "Dumb" 30 "expected" ["--dumb"] names]
   where
     names = failTestList ++ originalFailTestList
 
 intensiveTests :: TestTree
 intensiveTests =
-  testGroup
-    "Intensive"
-    [ testsWithParams "Dumb" 30 "expected" ["--dumb"] names
-    ]
+  testGroup "Intensive" [testsWithParams "Dumb" 30 "expected" ["--dumb"] names]
   where
     names = intensiveTestList ++ originalIntensiveTestList
 
 graphicsTests :: TestTree
 graphicsTests =
-  testGroup
-    "Graphics"
-    [ testsWithParams "Dumb" 30 "ppm" ["--dumb"] names
-    ]
+  testGroup "Graphics" [testsWithParams "Dumb" 30 "ppm" ["--dumb"] names]
   where
     names = graphicsTestList ++ originalGraphicsTestList
 
