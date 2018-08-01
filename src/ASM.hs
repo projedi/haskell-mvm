af73098716ed7a659ab12d37e836b012089cf87b
Alexander Shabalin <shabalyn.a@gmail.com>
2018-08-01 08:38:50 +0300

    Make ASM work in monad

--- src/ASM.hs
+++ src/ASM.hs
@@ -2,14 +2,15 @@ module ASM
   ( avenge
   ) where
 
+import Control.Monad.State (State, runState)
+
 import qualified ASMSyntax
 import qualified LinearSyntax
 
 avenge :: LinearSyntax.Program -> ASMSyntax.Program
 avenge p =
   ASMSyntax.Program
-    { ASMSyntax.programFunctions =
-        translateFunctionDef <$> LinearSyntax.programFunctions p
+    { ASMSyntax.programFunctions = fs
     , ASMSyntax.programLibraries = LinearSyntax.programLibraries p
     , ASMSyntax.programForeignFunctions = LinearSyntax.programForeignFunctions p
     , ASMSyntax.programConstants = LinearSyntax.programConstants p
@@ -19,52 +20,67 @@ avenge p =
     , ASMSyntax.programLastConstID = LinearSyntax.programLastConstID p
     , ASMSyntax.programLastLabelID = LinearSyntax.programLastLabelID p
     }
+  where
+    (fs, _) =
+      runState (mapM translateFunctionDef (LinearSyntax.programFunctions p)) Env
 
-translateFunctionDef :: LinearSyntax.FunctionDef -> ASMSyntax.FunctionDef
-translateFunctionDef fdef =
-  ASMSyntax.FunctionDef
-    { ASMSyntax.funDefRetType = LinearSyntax.funDefRetType fdef
-    , ASMSyntax.funDefName = LinearSyntax.funDefName fdef
-    , ASMSyntax.funDefParams = LinearSyntax.funDefParams fdef
-    , ASMSyntax.funDefLocals = LinearSyntax.funDefLocals fdef
-    , ASMSyntax.funDefBody = translateStatement <$> LinearSyntax.funDefBody fdef
-    }
+data Env =
+  Env
 
-translateStatement :: LinearSyntax.Statement -> ASMSyntax.Statement
+type ASM = State Env
+
+translateFunctionDef :: LinearSyntax.FunctionDef -> ASM ASMSyntax.FunctionDef
+translateFunctionDef fdef = do
+  body <- mapM translateStatement $ LinearSyntax.funDefBody fdef
+  pure $
+    ASMSyntax.FunctionDef
+      { ASMSyntax.funDefRetType = LinearSyntax.funDefRetType fdef
+      , ASMSyntax.funDefName = LinearSyntax.funDefName fdef
+      , ASMSyntax.funDefParams = LinearSyntax.funDefParams fdef
+      , ASMSyntax.funDefLocals = LinearSyntax.funDefLocals fdef
+      , ASMSyntax.funDefBody = body
+      }
+
+translateStatement :: LinearSyntax.Statement -> ASM ASMSyntax.Statement
 translateStatement (LinearSyntax.StatementFunctionCall fcall) =
-  ASMSyntax.StatementFunctionCall $ translateFunctionCall fcall
+  ASMSyntax.StatementFunctionCall <$> translateFunctionCall fcall
 translateStatement (LinearSyntax.StatementAssign v e) =
-  ASMSyntax.StatementAssign v $ translateExpr e
+  ASMSyntax.StatementAssign v <$> translateExpr e
 translateStatement (LinearSyntax.StatementAssignToPtr p v) =
-  ASMSyntax.StatementAssignToPtr p v
+  pure $ ASMSyntax.StatementAssignToPtr p v
 translateStatement (LinearSyntax.StatementReturn mv) =
-  ASMSyntax.StatementReturn mv
-translateStatement (LinearSyntax.StatementLabel l) = ASMSyntax.StatementLabel l
-translateStatement (LinearSyntax.StatementJump l) = ASMSyntax.StatementJump l
+  pure $ ASMSyntax.StatementReturn mv
+translateStatement (LinearSyntax.StatementLabel l) =
+  pure $ ASMSyntax.StatementLabel l
+translateStatement (LinearSyntax.StatementJump l) =
+  pure $ ASMSyntax.StatementJump l
 translateStatement (LinearSyntax.StatementJumpIfZero v l) =
-  ASMSyntax.StatementJumpIfZero v l
+  pure $ ASMSyntax.StatementJumpIfZero v l
 
-translateFunctionCall :: LinearSyntax.FunctionCall -> ASMSyntax.FunctionCall
+translateFunctionCall :: LinearSyntax.FunctionCall -> ASM ASMSyntax.FunctionCall
 translateFunctionCall fcall@LinearSyntax.NativeFunctionCall {} =
+  pure $
   ASMSyntax.NativeFunctionCall
     { ASMSyntax.nativeFunCallName = LinearSyntax.nativeFunCallName fcall
     , ASMSyntax.nativeFunCallRetType = LinearSyntax.nativeFunCallRetType fcall
     , ASMSyntax.nativeFunCallArgs = LinearSyntax.nativeFunCallArgs fcall
     }
 translateFunctionCall fcall@LinearSyntax.ForeignFunctionCall {} =
+  pure $
   ASMSyntax.ForeignFunctionCall
     { ASMSyntax.foreignFunCallName = LinearSyntax.foreignFunCallName fcall
     , ASMSyntax.foreignFunCallRetType = LinearSyntax.foreignFunCallRetType fcall
     , ASMSyntax.foreignFunCallArgs = LinearSyntax.foreignFunCallArgs fcall
     }
 
-translateExpr :: LinearSyntax.Expr -> ASMSyntax.Expr
+translateExpr :: LinearSyntax.Expr -> ASM ASMSyntax.Expr
 translateExpr (LinearSyntax.ExprFunctionCall fcall) =
-  ASMSyntax.ExprFunctionCall $ translateFunctionCall fcall
-translateExpr (LinearSyntax.ExprVar v) = ASMSyntax.ExprVar v
-translateExpr (LinearSyntax.ExprDereference v) = ASMSyntax.ExprDereference v
-translateExpr (LinearSyntax.ExprAddressOf v) = ASMSyntax.ExprAddressOf v
-translateExpr (LinearSyntax.ExprConst t c) = ASMSyntax.ExprConst t c
+  ASMSyntax.ExprFunctionCall <$> translateFunctionCall fcall
+translateExpr (LinearSyntax.ExprVar v) = pure $ ASMSyntax.ExprVar v
+translateExpr (LinearSyntax.ExprDereference v) =
+  pure $ ASMSyntax.ExprDereference v
+translateExpr (LinearSyntax.ExprAddressOf v) = pure $ ASMSyntax.ExprAddressOf v
+translateExpr (LinearSyntax.ExprConst t c) = pure $ ASMSyntax.ExprConst t c
 translateExpr (LinearSyntax.ExprBinOp op lhs rhs) =
-  ASMSyntax.ExprBinOp op lhs rhs
-translateExpr (LinearSyntax.ExprUnOp op v) = ASMSyntax.ExprUnOp op v
+  pure $ ASMSyntax.ExprBinOp op lhs rhs
+translateExpr (LinearSyntax.ExprUnOp op v) = pure $ ASMSyntax.ExprUnOp op v
