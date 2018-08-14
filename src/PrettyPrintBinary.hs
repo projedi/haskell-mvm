7a24b2b46b07793f9c334e3f84bf8af6ee8140f8
Alexander Shabalin <shabalyn.a@gmail.com>
2018-08-14 21:16:39 +0300

    Continue implementing

--- src/PrettyPrintBinary.hs
+++ src/PrettyPrintBinary.hs
@@ -7,7 +7,7 @@ import Data.IntMap (IntMap)
 import qualified Data.IntMap as IntMap
 import Data.Word (Word8)
 import qualified Foreign.C.String as Foreign
-import Foreign.Ptr (IntPtr(..), Ptr, castPtr, intPtrToPtr)
+import Foreign.Ptr (IntPtr(..), Ptr, castPtr, intPtrToPtr, plusPtr)
 import Foreign.Storable (peek)
 import Text.Printf (printf)
 
@@ -37,11 +37,19 @@ prettyPrintFunctions fs = (unlines . IntMap.elems) <$> mapM go fs
     go = pure . show
 
 prettyPrintCode :: IntMap Int64 -> Ptr () -> Int64 -> IO String
-prettyPrintCode ls p l = go (convertLabelMap ls) (castPtr p) l
+prettyPrintCode ls p l = go (convertLabelMap ls) (castPtr p) l 0
   where
-    go :: IntMap (Ptr Word8) -> Ptr Word8 -> Int64 -> IO String
-    go labels ptr len = _
-    convertLabelMap =
-      IntMap.fromList .
-      map (\(v1, v2) -> (fromIntegral v2, int64ToPtr $ fromIntegral v1)) .
-      IntMap.toList
+    go :: [Ptr Word8] -> Ptr Word8 -> Int64 -> Int -> IO String
+    go _ _ 0 _ = pure ""
+    go labels ptr len 10 = ("\n" ++) <$> go labels ptr len 0
+    go labels ptr len i =
+      let f =
+            if ptr `elem` labels
+              then (\x xs -> "\nlabel:\n" ++ x ++ xs)
+              else (++)
+       in f <$> printByte ptr <*> go labels (ptr `plusPtr` 1) (len - 1) (i + 1)
+    printByte :: Ptr Word8 -> IO String
+    printByte ptr = do
+      b <- peek ptr
+      pure $ printf "%x " b
+    convertLabelMap = map (int64ToPtr) . IntMap.elems
